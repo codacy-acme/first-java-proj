2669410630364daaf302a9c2041742a16e4b6782
David Marinho <david.marinho@codacy.com>
2022-04-07 17:40:11 +0100

    new java project in maven

---
+++ first-java-proj/src/main/java/com/codacy/tests/App.java
@@ -0,0 +1,18 @@
+package com.codacy.tests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    void sumNumbers()
+    {
+        System.out.println("The sum of 2 with 3 is :"+(2+3));
+    }
+  
+}
